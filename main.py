60bad7bf98370a76b5a11b4cc3207d46672124c4
Azadova Indira <aindira2197@gmail.com>
2026-02-08 16:33:00 +0500

    Add set operations and fruit addition examples

---
+++ main.py
@@ -0,0 +1,22 @@
+#1
+A = {1, 2, 3}
+B = {3, 4, 5}
+
+union_set = A | B   # yoki A.union(B)
+
+print(union_set)
+
+#2
+A = {1, 2, 3, 4}
+B = {3, 4}
+
+difference = A - B   # yoki A.difference(B)
+
+print(difference)
+
+#3
+fruits = {"apple", "banana"}
+fruits.add("orange")
+
+print(fruits)
+
